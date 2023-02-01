73a24bc9e301dd163095f9b706e73e3481502136
Wdnyana <purwawiadnyana08@gmail.com>
2023-02-01 10:02:51 +0800

    test branch connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <h1 className="text-shade-green">Hello World Test</h1>;
+  return (
+    <h1 className="text-shade-green container text-center">
+      Hello World Test & test branch
+    </h1>
+  );
 };
 
 export default App;
