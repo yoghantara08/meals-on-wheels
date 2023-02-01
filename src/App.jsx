71eb70378f84fe187709bec0961c16d9999c2a9d
wahyudwiprstka <wahyudwiprstka@gmail.com>
2023-02-01 11:29:34 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1 className="text-shade-green">Hello World Test</h1>;
+  return <h1 className="text-shade-green">Hello World</h1>;
 };
 
 export default App;
