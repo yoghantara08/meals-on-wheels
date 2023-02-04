770a4f294a0511c6d251ed8eec33979a52e60350
Yoghantara <gyoghantara@gmail.com>
2023-02-04 14:00:29 +0800

    admin API call with AXIOS

---
+++ src/api/constants.js
@@ -0,0 +1,14 @@
+export const ADMIN_URL = "http://localhost:8080/api/admin";
+export const AUTH_URL = "http://localhost:8080/api/auth";
+export const DONATION_URL = "http://localhost:8080/api/donation";
+export const MEALS_URL = "http://localhost:8080/api/meals";
+export const MEMBER_URL = "http://localhost:8080/api/member";
+export const PARTNER_URL = "http://localhost:8080/api/partner";
+export const RIDER_URL = "http://localhost:8080/api/rider";
+export const PROFILE_URL = "http://localhost:8080/api/profile";
+
+export const memberToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1lbWJlckBlbWFpbC5jb20iLCJ1c2VySWQiOiI2M2RiNzBmNGU1NTNlNThiNzUwZTIwYWIiLCJyb2xlIjoiTUVNQkVSIiwiaWF0IjoxNjc1NDg2Mjk2LCJleHAiOjE2NzU1NzI2OTZ9.xyaNM6RNOlsoV2-LNXpip4TbDve1OShf7P_1WS-kk3U";
+
+export const adminToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGVtYWlsLmNvbSIsInVzZXJJZCI6IjYzZGQxN2EyMmEyNGFlNWVhNzE0NDAwNCIsInJvbGUiOiJBRE1JTiIsImlhdCI6MTY3NTQ4NjMzOCwiZXhwIjoxNjc1NTcyNzM4fQ.TyEuI3EQjW6VtFg_sSxSDFYOrYkQ0KP6qJ_CqNPvOK4";
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { memberToken, adminToken } from "../../api/constants";
+import {
+  getUsers,
+  getPendingUsers,
+  acceptUsers,
+  getPartners,
+  getPendingPartners,
+  acceptPartners,
+  getMeals,
+  addMeal,
+  editMeal,
+  deleteMeal,
+  getOrderOnProgress,
+  getOrderComplete,
+  assignOrderToPartner,
+  assignOrderToRider,
+} from "../../api/admin-api";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const testAPI = () => {
+    assignOrderToRider(
+      adminToken,
+      "63dcb63d08330e47c906ddbf",
+      "63db7162dd210b73adc54bfa"
+    )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center mt-4">
+      <button className="btn btn-shade-yellow" onClick={testAPI}>
+        Test API
+      </button>
+    </div>
+  );
 };
 
 export default Dashboard;
